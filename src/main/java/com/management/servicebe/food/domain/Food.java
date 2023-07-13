e50985bc43a4d89357e3bf7a19ecba3c85ef1dd9
this-is-spear <rjsckdd12@gmail.com>
2023-07-14 02:47:03 +0900

    feature : Food 구현 및 테스트
    ###Description###
    
    동일한 유형의 파라미터로 인해 잘못된 주입이 발생할 경우를 대비해 생성자 및 게터 Test 진행

---
+++ src/main/java/com/management/servicebe/food/domain/Food.java
@@ -0,0 +1,60 @@
+package com.management.servicebe.food.domain;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Food {
+
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private String id;
+    @ToString.Include
+    private final String name;
+    @ToString.Include
+    private final Category category;
+    private final BigDecimal servingSizeGram;
+    private final BigDecimal caloriesKcal;
+    private final BigDecimal carbohydratesGram;
+    private final BigDecimal proteinGram;
+    private final BigDecimal fatGram;
+    private final BigDecimal sugarsGram;
+    private final BigDecimal sodiumMilliGram;
+    private final BigDecimal cholesterolMilliGram;
+    private final BigDecimal saturatedFattyAcidsGram;
+    private final BigDecimal transFattyAcidsGram;
+
+    public Food(String id, String name, Category category, BigDecimal servingSizeGram, BigDecimal caloriesKcal,
+                BigDecimal carbohydratesGram, BigDecimal proteinGram, BigDecimal fatGram, BigDecimal sugarsGram,
+                BigDecimal sodiumMilliGram, BigDecimal cholesterolMilliGram, BigDecimal saturatedFattyAcidsGram,
+                BigDecimal transFattyAcidsGram) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.servingSizeGram = servingSizeGram;
+        this.caloriesKcal = caloriesKcal;
+        this.carbohydratesGram = carbohydratesGram;
+        this.proteinGram = proteinGram;
+        this.fatGram = fatGram;
+        this.sugarsGram = sugarsGram;
+        this.sodiumMilliGram = sodiumMilliGram;
+        this.cholesterolMilliGram = cholesterolMilliGram;
+        this.saturatedFattyAcidsGram = saturatedFattyAcidsGram;
+        this.transFattyAcidsGram = transFattyAcidsGram;
+    }
+
+    @Builder
+    public Food(String name, Category category, BigDecimal servingSizeGram, BigDecimal caloriesKcal,
+                BigDecimal carbohydratesGram, BigDecimal proteinGram, BigDecimal fatGram, BigDecimal sugarsGram,
+                BigDecimal sodiumMilliGram, BigDecimal cholesterolMilliGram, BigDecimal saturatedFattyAcidsGram,
+                BigDecimal transFattyAcidsGram) {
+        this(null, name, category, servingSizeGram, caloriesKcal, carbohydratesGram, proteinGram, fatGram,
+                sugarsGram, sodiumMilliGram, cholesterolMilliGram, saturatedFattyAcidsGram, transFattyAcidsGram);
+    }
+}
---
+++ src/test/java/com/management/servicebe/food/domain/FoodTest.java
@@ -0,0 +1,61 @@
+package com.management.servicebe.food.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class FoodTest {
+
+    @Test
+    @DisplayName("""
+            식품은 식품 이름, 카테고리, 1회 제공량(g), 열량(kcal), 탄수화물(g), 단백질(g), 지방(g), 당류(g), 나트륨(mg), 콜레스테롤(mg), 포화지방산(g), 트랜스지방산(g)으로 구성된다.
+            """)
+    void createFood() {
+        // given
+        var name = "바나나칩";
+        var cholesterol = BigDecimal.ZERO;
+        var servingSize = BigDecimal.valueOf(30.0);
+        var calories = BigDecimal.valueOf(49.5);
+        var carbohydrates = BigDecimal.valueOf(5.4);
+        var protein = BigDecimal.valueOf(0.3);
+        var sugar = BigDecimal.valueOf(3.3);
+        var sodium = BigDecimal.valueOf(0.6);
+        var saturatedFattyAcidsG = BigDecimal.valueOf(2.7);
+        var transFattyAcids = BigDecimal.ZERO;
+        var category = Category.FOOD;
+
+        // when
+        var 바나나칩 = Food.builder()
+                .name(name)
+                .category(category)
+                .servingSizeGram(servingSize)
+                .caloriesKcal(calories)
+                .carbohydratesGram(carbohydrates)
+                .proteinGram(protein)
+                .sugarsGram(sugar)
+                .sodiumMilliGram(sodium)
+                .cholesterolMilliGram(cholesterol)
+                .saturatedFattyAcidsGram(saturatedFattyAcidsG)
+                .transFattyAcidsGram(transFattyAcids)
+                .build();
+
+        // then
+        assertAll(
+                () -> assertThat(바나나칩.getName()).isEqualTo(name),
+                () -> assertThat(바나나칩.getCategory()).isEqualTo(category),
+                () -> assertThat(바나나칩.getServingSizeGram()).isEqualTo(servingSize),
+                () -> assertThat(바나나칩.getCaloriesKcal()).isEqualTo(calories),
+                () -> assertThat(바나나칩.getCarbohydratesGram()).isEqualTo(carbohydrates),
+                () -> assertThat(바나나칩.getProteinGram()).isEqualTo(protein),
+                () -> assertThat(바나나칩.getSugarsGram()).isEqualTo(sugar),
+                () -> assertThat(바나나칩.getSodiumMilliGram()).isEqualTo(sodium),
+                () -> assertThat(바나나칩.getCholesterolMilliGram()).isEqualTo(cholesterol),
+                () -> assertThat(바나나칩.getSaturatedFattyAcidsGram()).isEqualTo(saturatedFattyAcidsG),
+                () -> assertThat(바나나칩.getTransFattyAcidsGram()).isEqualTo(transFattyAcids)
+        );
+    }
+}
