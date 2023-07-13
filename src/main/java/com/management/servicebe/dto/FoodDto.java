d661c2fd7ea341081d1cdc5dbf5dded43cf54997
youngreal <59333182+youngreal@users.noreply.github.com>
2023-07-13 09:20:55 +0900

    remove: #4 - food 연관없는부분 전부 삭제

--- src/main/java/com/management/servicebe/dto/FoodDto.java
+++ src/main/java/com/management/servicebe/dto/FoodDto.java
@@ -1,12 +1,6 @@
 package com.management.servicebe.dto;
 
-import com.management.servicebe.food.domain.Food;
-
 public record FoodDto(
         String name
 ) {
-
-    public static FoodDto toDto(Food food) {
-        return new FoodDto(food.getName());
-    }
 }
