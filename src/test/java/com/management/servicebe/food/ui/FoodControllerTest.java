0ac41bf99635504a10159a026c4b2f9efc20d60d
this-is-spear <rjsckdd12@gmail.com>
2023-07-15 11:49:26 +0900

    refact : FoodControllerTest 수정
    ###Description###
    
    - 사용하지 않는 익스텐션 삭제
    - Mockito -> BDDMockito 라이브러리로 변경
    - var 타입 활용

--- src/test/java/com/management/servicebe/food/ui/FoodControllerTest.java
+++ src/test/java/com/management/servicebe/food/ui/FoodControllerTest.java
@@ -3,21 +3,17 @@ package com.management.servicebe.food.ui;
 import com.management.servicebe.food.application.FoodQueryService;
 import com.management.servicebe.food.dto.FoodDetailResponse;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static com.management.servicebe.food.stubs.FoodStub.바나나칩;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@ExtendWith(MockitoExtension.class)
 @WebMvcTest(controllers = FoodController.class)
 class FoodControllerTest {
     @MockBean
@@ -27,13 +23,12 @@ class FoodControllerTest {
 
     @Test
     void findFoodDetail() throws Exception {
-        when(foodQueryService.findFoodDetail(1L)).thenReturn(FoodDetailResponse.of(바나나칩));
+        given(foodQueryService.findFoodDetail(1L)).willReturn(FoodDetailResponse.of(바나나칩));
 
-        ResultActions actions = mockMvc.perform(
+        var actions = mockMvc.perform(
                 get("/foods/{id}", 1)
         );
 
-        assertThat(actions.andReturn().getResponse().getStatus())
-                .isEqualTo(HttpStatus.OK.value());
+        assertThat(actions.andReturn().getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 }
