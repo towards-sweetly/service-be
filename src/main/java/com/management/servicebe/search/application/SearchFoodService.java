5bf14fbfd8a983c4636fa417cb5cf3958ea0f497
keonchang Lee <rjsckdd12@gmail.com>
2023-07-16 03:35:08 +0900

    fix : spring data 라이브러리 의존 코드 제거

--- src/main/java/com/management/servicebe/search/application/SearchFoodService.java
+++ src/main/java/com/management/servicebe/search/application/SearchFoodService.java
@@ -2,11 +2,11 @@ package com.management.servicebe.search.application;
 
 import com.management.servicebe.dto.FoodDto;
 import com.management.servicebe.food.application.SearchingFoodService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.util.Collections;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 //todo 해당 클래스의 역할이 단순 위임인게 조금 아쉬운데 나중에 해당 의존을 스킵하던지(SearchController -> FoodService) 유지하던지 한다.
 
@@ -16,11 +16,11 @@ public class SearchFoodService {
 
     private final SearchingFoodService searchingFoodService;
 
-    public List<FoodDto> searchFoods(String searchWord, Pageable pageable) {
+    public List<FoodDto> searchFoods(String searchWord) {
         if (searchWord == null || searchWord.isBlank()) {
             return Collections.emptyList();
         }
 
-        return searchingFoodService.searchFoods(searchWord, pageable);
+        return searchingFoodService.searchFoods(searchWord);
     }
 }
